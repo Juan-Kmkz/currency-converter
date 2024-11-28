945098856b466cf51c4375bbeac79355b383dd9a
Juan-Kmkz <juandanilo777@gmail.com>
2024-11-27 22:25:22 -0300

    funcionalidade de alteracao automatica do valor convertido adicionada

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const calculationButton = document.querySelector("#operation-button")
 const currencySelectToConvert = document.getElementById("select-two")
+const iconDolar = document.getElementById("icon-dolar")
 // const valueEntered = document.getElementById("value-to-convert")
 
 
@@ -29,10 +30,11 @@ function convertValues(){
         currencyValueConverted.innerText = new Intl.NumberFormat("en-UK", {style:"currency", currency:"EUR"}).format(valueEntered / euroToday) //quando o select for "euro"
     }
 
+    if(currencySelectToConvert == "euro"){iconDolar.innerHTML.src = "./assets.euro.png"}
+
+    if(currencySelectToConvert == "dolar"){iconDolar.innerHTML.src="./assets.dolar.png"}
+
     // if (currencySelectToConvert.value == "libra"){}
-
-
-
 
 console.log(currencySelectToConvert.value)
 
@@ -42,6 +44,6 @@ console.log(currencySelectToConvert.value)
 function toctoc(){
 console.log("agr conectou")
 }
-
+currencySelectToConvert.addEventListener("change",convertValues)
 calculationButton.addEventListener("click",convertValues)
 // valueEntered.addEventListener("keypress",toctoc)
