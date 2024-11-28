30b45667c3a18251c33c30600abd9853dd7dcde7
Juan-Kmkz <juandanilo777@gmail.com>
2024-11-27 21:10:07 -0300

    conversao para dolar e euro operacional, pendente a alteracao dos icones, e a conversao automatica ao trocar as moedas

--- script.js
+++ script.js
@@ -1,7 +1,47 @@
-const abacate = document.querySelector("#operation-button")
+const calculationButton = document.querySelector("#operation-button")
+const currencySelectToConvert = document.getElementById("select-two")
+// const valueEntered = document.getElementById("value-to-convert")
+
+
+const dolarToday = 5.81
+const euroToday = 6.10
+const libraToday = 7.30
+const bitcoinToday = 534369.25
+
+
+
+
+function convertValues(){
+    const valueEntered = document.getElementById("value-to-convert").value
+    const currencyValueToConvert = document.getElementById("value-for-conversion")
+    const currencyValueConverted = document.getElementById("converted-value")
+
+    // const currencyConversionDolar = valueEntered / dolarToday
+    // const currencyConversionEuro  = valueEntered / euroToday
+ 
+    currencyValueToConvert.innerText = new Intl.NumberFormat("pt-BR", {style:"currency", currency:"BRL"}).format(valueEntered)
+
+    if (currencySelectToConvert.value == "dolar"){
+        currencyValueConverted.innerText = new Intl.NumberFormat("en-US", {style:"currency", currency:"USD"}).format(valueEntered / dolarToday) //quando o select for "dolar..."
+    }
+
+    if (currencySelectToConvert.value == "euro"){
+        currencyValueConverted.innerText = new Intl.NumberFormat("en-UK", {style:"currency", currency:"EUR"}).format(valueEntered / euroToday) //quando o select for "euro"
+    }
+
+    // if (currencySelectToConvert.value == "libra"){}
+
+
+
+
+console.log(currencySelectToConvert.value)
+
+} 
+
 
 function toctoc(){
 console.log("agr conectou")
 }
 
-abacate.addEventListener("click",toctoc)
+calculationButton.addEventListener("click",convertValues)
+// valueEntered.addEventListener("keypress",toctoc)
